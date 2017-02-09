58e29e73c60b0dffcceca30991d0bc9e0cd23239
Angel Paunchev <apaunchev@gmail.com>
2017-02-09 21:30:21 +0200

    feat(app): fetch only featured photos

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const APP_ID = '1997df71664984f20c0b1b8fcd42d1279fb98c424e07e0e1385299eed6935142'
-const RANDOM_PHOTO_ENDPOINT = `https://api.unsplash.com/photos/random?client_id=${APP_ID}`
+const RANDOM_PHOTO_ENDPOINT = `https://api.unsplash.com/photos/random?featured=true&client_id=${APP_ID}`
 
 export { RANDOM_PHOTO_ENDPOINT }
