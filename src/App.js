0323b034f41e4d4543c31a0e30413cab62dbf3eb
Angel Paunchev <apaunchev@gmail.com>
2018-11-25 18:23:58 +0200

    optimize fetching, handle errors, use collections

--- src/App.js
+++ src/App.js
@@ -2,53 +2,83 @@ import React, { useState, useEffect } from "react";
 import NProgress from "nprogress";
 
 import { fetchJSONData, fetchImageData } from "./utils";
-import { LOCAL_STORAGE_KEY, API_URL } from "./consts";
 
 import "../node_modules/primer/build/build.css";
 import "../node_modules/nprogress/nprogress.css";
 import "./App.css";
 
+const ENDPOINTS = {
+  RANDOM: "https://api.unsplash.com/photos/random?",
+  SEARCH: "https://unsplash.com/search"
+};
+const LOCAL_STORAGE_KEY = {
+  NEXT_PHOTO: "ds_nextPhoto",
+  COLLECTIONS: "ds_collections"
+};
+const collections = () => {
+  try {
+    return window.localStorage.getItem(LOCAL_STORAGE_KEY.COLLECTIONS);
+  } catch (e) {
+    // default to Unsplash Editorial collection (https://unsplash.com/collections/317099)
+    return ["317099"];
+  }
+};
+const requestParams = {
+  collections,
+  orientation: "landscape",
+  w: window.innerWidth,
+  h: window.innerHeight,
+  client_id: process.env.REACT_APP_UNSPLASH_CLIENT_ID
+};
+
 const App = () => {
-  const [photo, setPhoto] = useState({});
-  const cachedPhoto = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-  const fetchPhoto = async () => {
-    NProgress.start();
-
+  const initialPhoto = () =>
+    JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY.NEXT_PHOTO)) || {};
+  const [photo, setPhoto] = useState(initialPhoto);
+  const [loading, setLoading] = useState(false);
+  const getPhoto = async () => {
     try {
-      const photo = await fetchJSONData(API_URL);
-      try {
-        let image = await fetchImageData(photo.urls.custom);
-        image = { ...photo, image };
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(image));
-        NProgress.done();
-        return image;
-      } catch (e) {
-        return console.error(e);
-      }
+      setLoading(true);
+      NProgress.start();
+      const queryString = Object.keys(requestParams)
+        .map(key => `${key}=${requestParams[key]}`)
+        .join("&");
+      const photo = await fetchJSONData(`${ENDPOINTS.RANDOM}${queryString}`);
+      const data = await fetchImageData(photo.urls.custom || photo.urls.full);
+      setPhoto({
+        id: photo.id,
+        links: photo.links,
+        location: photo.location,
+        user: photo.user,
+        data
+      });
     } catch (e) {
-      return console.error(e);
+      console.error(`Error fetching photo: ${e}`);
+      setPhoto({
+        user: {
+          name: "Unable to load photo."
+        },
+        location: {
+          title: "Please try again or contact the developer."
+        }
+      });
+    } finally {
+      setLoading(false);
+      NProgress.done();
     }
   };
 
-  useEffect(
-    () => {
-      (async () => {
-        try {
-          if (cachedPhoto) {
-            setPhoto(JSON.parse(cachedPhoto));
-          } else {
-            setPhoto(await fetchPhoto());
-          }
-        } catch (e) {
-          return console.error(e);
-        }
-      })();
-    },
-    [setPhoto]
-  );
+  useEffect(() => {
+    (async () => {
+      if (!initialPhoto().id) await getPhoto();
+    })();
+  }, []);
 
-  if (!photo || !photo.id) {
+  useEffect(() => localStorage.setItem(LOCAL_STORAGE_KEY.NEXT_PHOTO, JSON.stringify(photo)), [
+    photo
+  ]);
+
+  if (!photo) {
     return <div className="App" />;
   }
 
@@ -57,25 +87,32 @@ const App = () => {
       className="App d-flex flex-items-end"
       style={{
         backgroundImage: `linear-gradient(180deg, transparent 80%, rgba(0, 0, 0, 0.3)), url(${
-          photo.image
+          photo.data
         })`
       }}
     >
       <div className="d-flex flex-auto flex-justify-between flex-items-center p-3">
         <div className="d-flex flex-column">
-          <div>
-            <a className="text-white" href={photo.user.links.html}>
-              {photo.user.name}
-            </a>
-          </div>
+          {photo.user && (
+            <div>
+              {photo.user.links ? (
+                <a className="text-white" href={photo.user.links.html}>
+                  {photo.user.name}
+                </a>
+              ) : (
+                <span className="text-white">{photo.user.name}</span>
+              )}
+            </div>
+          )}
           {photo.location && (
             <div>
-              <a
-                className="text-white f6"
-                href={`https://unsplash.com/search/${photo.location.name}`}
-              >
-                {photo.location.title}
-              </a>
+              {photo.location.name ? (
+                <a className="text-white f6" href={`${ENDPOINTS.SEARCH}/${photo.location.name}`}>
+                  {photo.location.title}
+                </a>
+              ) : (
+                <span className="text-white f6">{photo.location.title}</span>
+              )}
             </div>
           )}
         </div>
@@ -83,13 +120,16 @@ const App = () => {
           <div className="BtnGroup">
             <button
               className="btn BtnGroup-item"
-              onClick={async () => setPhoto(await fetchPhoto())}
+              onClick={async () => await getPhoto()}
+              disabled={loading}
             >
-              Shuffle
+              Refresh
             </button>
-            <a className="btn BtnGroup-item" href={photo.links.download}>
-              Download
-            </a>
+            {photo.links && (
+              <a className="btn BtnGroup-item" href={photo.links.download}>
+                Download
+              </a>
+            )}
           </div>
         </div>
       </div>
